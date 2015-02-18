32ba2c3af2230bf3508ff137fa9be895f09ade3a
Allison Marino <amarino_us@yahoo.com>
2015-02-17 16:59:12 -0800

    add task table connected to events

--- who_brings/app/controllers/events_controller.rb
+++ who_brings/app/controllers/events_controller.rb
@@ -9,11 +9,21 @@ class EventsController < ApplicationController
 	end
 
 	def show
-
+			event_id = params[:event_id]
+		@event = event.find(event_id)
+		respond_to do |format|
+			format.html
+			format.json { render json: @tasks }
+		end
 	end
 
 	def create
-
+		new_event = params.require(:event).permit(:title, :description, :start_date, :end_date, :start_time, :end_time)
+		event = Event.create(new_event)
+		respond_to do |format|
+			format.html { redirect_to "/events#event.id" }
+			format.json { render json: @event }
+		end
 	end
 
 	def edit
--- who_brings/app/controllers/tasks_controller.rb
+++ who_brings/app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
-	before_action :get_user 
-	before_action :logged_in?, :authorized_user?, only: [:index, :new, :show, :edit]
+	before_action :get_event
+	# before_action :logged_in?, :authorized_user?, only: [:index, :new, :show, :edit]
 
 	def index
-		@tasks = @user.tasks.all
+		@tasks = @event.tasks.all
+		
+		# user_id = params[:user_id]
+		# @user = User.find(user_id)
 		# render :index
-
 		respond_to do |format|
 			format.html { render :index}
 			format.json { render json: @tasks }
@@ -18,17 +20,18 @@ class TasksController < ApplicationController
 	end
 
 	def create
-		new_task = params.require(:task).permit(:content, :complete)
-		@task = @user.tasks.create(new_task)
+		new_task = params.require(:task).permit(:content, :user_id, :cost, :complete)
+		@task = @event.tasks.create(new_task)
 		respond_to do |format|
-			format.html { redirect_to "/users/#{@user.id}/tasks/#{task.id}" }
+			# format.html { redirect_to "/users/#{@user.id}/tasks/#{task.id}" }
+			format.html { redirect_to "/events/#{@event.id}/tasks/#{@task.id}" }
 			format.json { render json: @task }
 		end
 	end
 
 	def show
 		task_id = params[:task_id]
-		@task = @user.tasks.find(task_id)
+		@task = @event.tasks.find(task_id)
 		respond_to do |format|
 			format.html
 			format.json { render json: @tasks }
@@ -37,13 +40,13 @@ class TasksController < ApplicationController
 
 	def edit
 		task_id = params[:task_id]
-		@task = @user.tasks.find(task_id)
+		@task = @event.tasks.find(task_id)
 		render :edit
 	end
 
 	def update
 		task_id = params[:task_id]
-		task = @user.tasks.find(task_id)
+		task = @event.tasks.find(task_id)
 		updated_attrs = params.require(:task).permit(:content, :complete)
 		task.update_attributes(updated_attrs)
 		# redirect_to task_path
@@ -56,7 +59,7 @@ class TasksController < ApplicationController
 
 	def destroy
 		id = params[:task_id]
-		task = @user.tasks.find(id)
+		task = @event.tasks.find(id)
 		task.destroy
 
 		respond_to do |format|
@@ -66,9 +69,9 @@ class TasksController < ApplicationController
 	end
 
 	private
-		def get_user
-			user_id = params[:user_id]
-			@user = User.find(user_id)
+		def get_event
+			event_id = params[:event_id]
+			@event = Event.find(event_id)
 		end
 
 end
